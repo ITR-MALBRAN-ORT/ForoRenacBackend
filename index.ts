0b22ce7d6ae17b68afc3665fc51507ed1cd0f589
Andrea Puentes <Andrea_Puentes@intranet.itresources>
2022-10-14 11:15:37 -0300

    feat(feat/new_pr)

--- index.ts
+++ index.ts
@@ -1,12 +1,29 @@
 import express, { Express } from 'express';
 import config from 'config';
+import { ValidationError } from 'express-validation';
 import indexRouter from './src/routes/index.routes';
+import registerErrorHandling from './src/middlewares/error.middlewares';
 
 const PORT: number = Number(config.get('server.port') ?? 3000);
 
 const app: Express = express();
 
 app.use('/api/v1', indexRouter);
+
+app.use((err: any, req: any, res: any, next: any) => {
+  if (err instanceof ValidationError)
+    registerErrorHandling(
+      {
+        code: err.statusCode,
+        message:
+          Array.isArray(err.details.body) && err.details.body.length > 0
+            ? err.details.body[0].message
+            : '',
+      },
+      req,
+      res,
+    );
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
