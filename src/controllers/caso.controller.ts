cd4776cca6aecad8379f0be8e31d04f17ea7281e
Matiiiiiiiiiiiiiiiiiiiiiiiiiiiiii <102532863+MatiMCode@users.noreply.github.com>
2022-11-25 10:15:33 -0300

    feat/CU_007_BE_Creación_de_Caso: controller and router created

--- src/controllers/caso.controller.ts
+++ src/controllers/caso.controller.ts
@@ -4,13 +4,37 @@ import Usuarios from "../models/usuarios";
 import { compare, hash } from 'bcrypt';
 
     const  crearCaso = async (___req: Request, ___res: Response) => {
-        const datosMadre = await Usuarios.findAll({
-            where:{
-                mail:___req.body.mail
-            }
-        })
-        
-        console.log(___req.body.password)
+        const datosMadre = {
+            nombre:___req.body.nombreMadre,
+            apellido:___req.body.apellidoMadre,
+            dni:___req.body.dniMadre,
+            nro_hc:___req.body.numeroHistoriaClinicaMadre,
+            edad:___req.body.edadMadre,
+            domicilio_calle:___req.body.domicilioCalleMadre,
+            domicilio_numero:___req.body.domicilioNumeroMadre,
+            domicilio_localidad:___req.body.domicilioLocalidadMadre,
+            domicilio_pcia:___req.body.domicilioPciaMadre,
+            telefono: ___req.body.telefonoMadre
+        }
+
+        const datosBebe = {
+            nombre:___req.body.nombreBebe,
+            apellido:___req.body.apellidoBebe,
+            dni:___req.body.dniBebe,
+            nro_hc:___req.body.numeroHistoriaClinicaBebe,
+            fecha_nacimiento:___req.body.fechaNacimientoBebe,
+            nacido:___req.body.nacidoBebe,
+            alta:___req.body.altaBebe,
+            sexo:___req.body.sexoBebe,
+            peso:___req.body.pesoBebe,
+            talla:___req.body.tallaBebe,
+            pc:___req.body.pcBebe,
+            eg:___req.body.egBebe,
+            gestas:___req.body.gestasBebe,
+            gemelo:___req.body.gemeloBebe,
+            con_alta:___req.body.conAltaBebe,
+            hosp_derivado:___req.body.hospitalDerivadoBebe
+        }
 
 
 
